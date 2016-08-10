65bf7f86c0e0ed77c600b8e0859fff7445dc0ff3
ricfrank <riccardo@MBPdiRiccardo2.station>
2016-08-10 14:22:03 +0200

    add createStore implementation from scratch

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import expect, { createSpy, spyOn, isSpy } from 'expect'
-import { createStore } from 'redux'
 
 const counter = (state = 0, action) => {
     if(action.type === 'INCREMENT') {
@@ -10,7 +9,31 @@ const counter = (state = 0, action) => {
         return state - 1;
     }
     return state;
-}
+};
+
+
+const createStore = (reducer) => {
+    let state;
+    let listeners = [];
+
+    const getState = () => state;
+
+    const dispatch = (action) => {
+        state = reducer(state, action);
+        listeners.forEach(listener => listener());
+    };
+
+    const subscribe = (listener) => {
+        listeners.push(listener);
+        return () => {
+            listeners = listeners.filter(l => l !== listener);
+        };
+    };
+
+    dispatch({});
+
+    return {getState, dispatch, subscribe};
+};
 
 const store = createStore(counter);
 

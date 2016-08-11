ee7fdffbdc9a36fdd0e4321346373ff916bbd40f
ricfrank <riccardo@MBPdiRiccardo2.station>
2016-08-11 16:52:05 +0200

    implement todo list application with React - ADD TODO

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import {createStore, combineReducers} from 'redux'
+import React, {Component} from 'react'
+import ReactDom from 'react-dom'
 
 const todo = (state, action) => {
     switch (action.type) {
@@ -33,8 +35,6 @@ const todos = (state = [], action) => {
             return state.map(t => {
                 return todo(t, action)
             });
-            //same
-            // return state.map(t => todo(t, action));
         default:
             return state;
     }
@@ -52,71 +52,53 @@ const visibilityFilter = (
     }
 };
 
-//only if the key name is the same of value name
 const todoApp = combineReducers({
     todos,
     visibilityFilter
 });
 
-//same
-// const todoApp = combineReducers({
-//     todos: todos,
-//     visibilityFilter: visibilityFilter
-// });
-
-//old
-// const todoApp = (state = {}, action) => {
-//     return {
-//         todos: todos(
-//             state.todos, action
-//         ),
-//         visibilityFilter: visibilityFilter(
-//             state.visibilityFilter,
-//             action
-//         )
-//     };
-// };
 
 const store = createStore(todoApp);
 
-console.log('Initial state:');
-console.log(store.getState());
-console.log('--------------');
+let nextTodoId = 0;
 
-console.log('Dispatching ADD_TODO.');
-store.dispatch({
-    type: 'ADD_TODO',
-    id: 0,
-    text: 'Learn Redux'
-});
-console.log('Current state:');
-console.log(store.getState());
-console.log('--------------');
+class TodoApp extends Component {
+    render() {
+        return (
+            <div>
+                <input ref={note => {
+                    this.input = note;
+                }}/>
+                <button onClick={() => {
+                    store.dispatch({
+                        type: 'ADD_TODO',
+                        text: this.input.value,
+                        id: nextTodoId++
+                    });
+                    this.input.value = '';
+                }}>
+                    Add Todo
+                </button>
+                <ul>
+                    {this.props.todos.map(todo =>
+                        <li key={todo.id}>
+                            {todo.text}
+                        </li>
+                    )}
+                </ul>
+            </div>
+        )
+    }
+}
 
-console.log('Dispatching ADD_TODO.');
-store.dispatch({
-    type: 'ADD_TODO',
-    id: 1,
-    text: 'Hey Hey!'
-});
-console.log('Current state:');
-console.log(store.getState());
-console.log('--------------');
+const render = () => {
+    ReactDom.render(
+        <TodoApp
+            todos={store.getState().todos}
+        />,
+        document.getElementById('root')
+    );
+};
 
-console.log('Dispatching TOGGLE_TODO.');
-store.dispatch({
-    type: 'TOGGLE_TODO',
-    id: 0
-});
-console.log('Current state:');
-console.log(store.getState());
-console.log('--------------');
-
-console.log('Dispatching SET_VISIBILITY_FILTER.');
-store.dispatch({
-    type: 'SET_VISIBILITY_FILTER',
-    filter: 'SHOW_COMPLETED'
-});
-console.log('Current state:');
-console.log(store.getState());
-console.log('--------------');
+store.subscribe(render);
+render();

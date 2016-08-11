db80af714ddc084971a029d7710b118f2bc35c00
ricfrank <riccardo@MBPdiRiccardo2.station>
2016-08-11 09:57:57 +0200

    implement todolist reducer with unit test - TOGGLE TODO

--- src/main.js
+++ src/main.js
@@ -12,6 +12,17 @@ const todos = (state = [], action) => {
                     completed: false
                 }
             ];
+        case 'TOGGLE_TODO':
+            return state.map(todo => {
+                if (todo.id !== action.id) {
+                    return todo
+                }
+
+                return {
+                    ...todo,
+                    completed: !todo.completed
+                };
+            });
         default:
             return state;
     }
@@ -39,5 +50,43 @@ const testAddTodo = () => {
     ).toEqual(stateAfter);
 };
 
+const testToggleTodo = () => {
+    const stateBefore = [
+        {
+            id: 0,
+            text: 'Learn Redux',
+            completed: false
+        },
+        {
+            id: 1,
+            text: 'Hey Hey',
+            completed: false
+        }
+    ];
+    const action = {
+        type: 'TOGGLE_TODO',
+        id: 0,
+    };
+    const stateAfter = [
+        {
+            id: 0,
+            text: 'Learn Redux',
+            completed: true
+        },
+        {
+            id: 1,
+            text: 'Hey Hey',
+            completed: false
+        }
+    ];
+    deepFreeze(stateBefore);
+    deepFreeze(action);
+
+    expect(
+        todos(stateBefore, action)
+    ).toEqual(stateAfter);
+};
+
 testAddTodo();
+testToggleTodo();
 console.log('All tests passed');
